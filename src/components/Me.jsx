211b7bad47202c8ab90a03070ce22c990d807328
Tariqul Islam <youandtariq@gmail.com>
2021-07-06 17:13:00 +0600

    minor change in links

--- src/components/Me.jsx
+++ src/components/Me.jsx
@@ -1,10 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Me = () => {
   return (
     <section className="me-section">
       <div className="me">
         <h2 style={{ textAlign: "center", color: "red" }}>Coming Soon......</h2>
+        <div>
+          <Link to="/resultUpload">Upload Result</Link>
+          <Link to="/latestNewsUpload">Update Latest News</Link>
+        </div>
       </div>
     </section>
   );
